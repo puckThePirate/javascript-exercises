d8f304d7244a58227b14e897f695b604f8134ec6
Vasu Vats <vasuvats2800@gmail.com>
2022-06-15 20:54:23 +0530

    "solved tempConversion.js"

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = (5/9)*(temp-32);
+  return parseFloat(newTemp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+ let newTemp = (9/5)*(temp) + 32;
+  return parseFloat(newTemp.toFixed(1));
 };
 
 // Do not edit below this line
