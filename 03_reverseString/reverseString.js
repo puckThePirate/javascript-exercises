64100d58f409a05e181003512478527e61bc32ea
Vasu Vats <vasuvats2800@gmail.com>
2022-06-15 19:26:04 +0530

    solved reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let rstr = '';
+    let arr = str.split('');
+    for(let i=str.length-1;i>=0;i--){
+        rstr = rstr + arr[i];
+    }
+    return rstr;
 };
 
 // Do not edit below this line
