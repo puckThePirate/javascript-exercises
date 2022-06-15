5278676bdb59757fad0e95e53e0607664dc8b95f
Vasu Vats <vasuvats2800@gmail.com>
2022-06-15 20:05:52 +0530

    solved sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+let sum = 0;
+if(a<0||b<0||typeof(a)!='number' || typeof(b)!='number') return 'ERROR';
+else {
+    let x = Math.max(a,b);
+    let y = Math.min(a,b);
+for(let i=y;i<=x;i++){
+    sum = sum + i;
+}
+}
+return sum;
 };
 
 // Do not edit below this line
