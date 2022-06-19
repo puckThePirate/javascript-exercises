99d7c418181db7739d0eed1e985872f86fa1ce30
Vasu Vats <vasuvats2800@gmail.com>
2022-06-19 23:40:14 +0530

    solved getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+const titles = arr.reduce((tit,entry) => {
+    tit.splice(tit.length,1,entry.title);
+    return tit;
+},[]);
+return titles;
 };
 
 // Do not edit below this line
