a5556d2d75bc29acbe989bc7594a9ef7e0122e44
Vasu Vats <vasuvats2800@gmail.com>
2022-06-19 18:23:33 +0530

    solved calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for(let i=0;i<arr.length;i++){
+    sum = sum + arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+ let prod = 1;
+ for(let i=0;i<arr.length;i++){
+  prod = prod * arr[i];
+}
+return prod;
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let pow = a;
+	for(let i=1;i<b;i++){
+    pow = pow*a;
+  }
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  for(let i=1;i<=a;i++){
+    fact = fact*i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
