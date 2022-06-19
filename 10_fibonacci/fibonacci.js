407f24a407748378e32b227d7e0744c7348b47eb
Vasu Vats <vasuvats2800@gmail.com>
2022-06-19 22:59:52 +0530

    solved fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 1) return "OOPS";
+    else {
+        let t1, t2, t3;
+        t1 = 1;
+        t2 = 1;
+        t3 = 1;
+        while (a-2>0) {
+            t3 = t2 + t1;
+            t1 = t2;
+            t2 = t3;
+            a--;
+        }
+        return t3;
+    }
 };
 
 // Do not edit below this line
